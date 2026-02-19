139f08e696748110dbabcec2e6e969c7f54e8fd7
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-02-19 17:31:30 +0530

    Add Subcription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,7 +204,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       secure: true
     }
   
-    const {accessToken,newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const {accessToken,refreshToken:newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
   
     return res
     .status(200)
@@ -221,7 +221,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     throw new ApiError(401, error?.message || "Invalid refresh token")
   }
 })
-
 
 
 export {
